8d904edacd8a4c4c25454b5ee2454c9adc3ee26b
Darbi <darbiadixon@gmail.com>
2026-05-06 18:43:47 -0600

    Improve single responsibility in PawnMovesCalculator

--- shared/src/main/java/chess/moves/calculator/PawnMovesCalculator.java
+++ shared/src/main/java/chess/moves/calculator/PawnMovesCalculator.java
@@ -9,72 +9,67 @@ public class PawnMovesCalculator implements PieceMovesCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         ChessPiece piece = board.getPiece(position);
-        chess.ChessGame.TeamColor color = piece.getTeamColor();
+        ChessGame.TeamColor color = piece.getTeamColor();
         Collection<ChessMove> possibleMoves = new ArrayList<>();
-        Collection<ChessPosition> newPositions = new ArrayList<>();
+        Collection<ChessPosition> possiblePositions = new ArrayList<>();
 
-        int row = position.getRow();
-        int col = position.getColumn();
+        validateStraight(possiblePositions, position, board, color);
+        validateDiagonals(possiblePositions, position, board, color);
 
-        if (color == ChessGame.TeamColor.WHITE) {
-            var firstPosition = new ChessPosition(row + 1, col);
-            newPositions.add(firstPosition);
-
-            if (row == 2) {
-                validateStraight(newPositions, board, firstPosition, 1);
-            }
-
-            var leftDiagonal = new ChessPosition(row + 1, col - 1);
-            var rightDiagonal = new ChessPosition(row + 1, col + 1);
-            validateDiagonal(newPositions, board, leftDiagonal);
-            validateDiagonal(newPositions, board, rightDiagonal);
-
-        } else if (color == ChessGame.TeamColor.BLACK) {
-            var firstPosition = new ChessPosition(row - 1, col);
-            newPositions.add(firstPosition);
-
-            if (row == 7) {
-                validateStraight(newPositions, board, firstPosition, -1);
-            }
-
-            var leftDiagonal = new ChessPosition(row - 1, col - 1);
-            var rightDiagonal = new ChessPosition(row - 1, col + 1);
-            validateDiagonal(newPositions, board, leftDiagonal);
-            validateDiagonal(newPositions, board, rightDiagonal);
-        }
-
-
-        for (ChessPosition newPosition : newPositions) {
-            if ((color == ChessGame.TeamColor.WHITE && newPosition.getRow() == 8)
-                    || (color == ChessGame.TeamColor.BLACK && newPosition.getRow() == 1)) {
-                pawnUpdatePossibleMoves(possibleMoves, position, newPosition, ChessPiece.PieceType.QUEEN, piece, board);
-                pawnUpdatePossibleMoves(possibleMoves, position, newPosition, ChessPiece.PieceType.BISHOP, piece, board);
-                pawnUpdatePossibleMoves(possibleMoves, position, newPosition, ChessPiece.PieceType.ROOK, piece, board);
-                pawnUpdatePossibleMoves(possibleMoves, position, newPosition, ChessPiece.PieceType.KNIGHT, piece, board);
+        for (ChessPosition possiblePosition : possiblePositions) {
+            if ((color == ChessGame.TeamColor.WHITE && possiblePosition.getRow() == 8)
+                    || (color == ChessGame.TeamColor.BLACK && possiblePosition.getRow() == 1)) {
+                pawnUpdatePossibleMoves(possibleMoves, position, possiblePosition, ChessPiece.PieceType.QUEEN, piece, board);
+                pawnUpdatePossibleMoves(possibleMoves, position, possiblePosition, ChessPiece.PieceType.BISHOP, piece, board);
+                pawnUpdatePossibleMoves(possibleMoves, position, possiblePosition, ChessPiece.PieceType.ROOK, piece, board);
+                pawnUpdatePossibleMoves(possibleMoves, position, possiblePosition, ChessPiece.PieceType.KNIGHT, piece, board);
             } else {
-                pawnUpdatePossibleMoves(possibleMoves, position, newPosition, null, piece, board);
+                pawnUpdatePossibleMoves(possibleMoves, position, possiblePosition, null, piece, board);
             }
         }
 
         return possibleMoves;
     }
 
-    private void validateDiagonal(Collection<ChessPosition> newPositions,
-                                  ChessBoard board, ChessPosition diagonal) {
-        if (!diagonal.outOfRange()) {
-            ChessPiece otherPiece = board.getPiece(diagonal);
-            if (otherPiece != null) {
-                newPositions.add(diagonal);
+    private void validateStraight(Collection<ChessPosition> possiblePositions, ChessPosition position, ChessBoard board, ChessGame.TeamColor color) {
+        int increment = 1;
+        int startingRow = 2;
+
+        if (ChessGame.TeamColor.BLACK == color) {
+            increment = -1;
+            startingRow = 7;
+        }
+
+        var firstPosition = new ChessPosition(position.getRow() + increment, position.getColumn());
+        possiblePositions.add(firstPosition);
+
+        if (position.getRow() == startingRow && !firstPosition.outOfRange()) {
+            ChessPiece otherPiece = board.getPiece(firstPosition);
+            if (otherPiece == null) {
+                possiblePositions.add(new ChessPosition(firstPosition.getRow() + increment, firstPosition.getColumn()));
             }
         }
     }
 
-    private void validateStraight(Collection<ChessPosition> newPositions,
-                                  ChessBoard board, ChessPosition firstPosition, int increment) {
-        if (!firstPosition.outOfRange()) {
-            ChessPiece otherPiece = board.getPiece(firstPosition);
-            if (otherPiece == null) {
-                newPositions.add(new ChessPosition(firstPosition.getRow() + increment, firstPosition.getColumn()));
+    private void validateDiagonals(Collection<ChessPosition> possiblePositions, ChessPosition position,
+                                   ChessBoard board, ChessGame.TeamColor color) {
+        int increment = 1;
+        if (ChessGame.TeamColor.BLACK == color) {
+            increment = -1;
+        }
+        ChessPosition firstDiagonal = new ChessPosition(position.getRow() + increment, position.getColumn() - 1);
+        ChessPosition secondDiagonal = new ChessPosition(position.getRow() + increment, position.getColumn() + 1);
+        validateDiagonal(possiblePositions, firstDiagonal, position, board);
+        validateDiagonal(possiblePositions, secondDiagonal, position, board);
+    }
+
+    private void validateDiagonal(Collection<ChessPosition> possiblePositions, ChessPosition diagonal,
+                                  ChessPosition position, ChessBoard board) {
+        if (!diagonal.outOfRange()) {
+            ChessPiece myPiece = board.getPiece(position);
+            ChessPiece otherPiece = board.getPiece(diagonal);
+            if (pawnCanCapture(myPiece, otherPiece, position, diagonal)) {
+                possiblePositions.add(diagonal);
             }
         }
     }
