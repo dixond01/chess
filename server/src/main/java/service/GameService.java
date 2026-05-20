6894dd4c15e1b11a4c60c745d1d1b6a27a4b5b8f
Darbi <darbiadixon@gmail.com>
2026-05-20 10:26:10 -0600

    Add service classes

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,7 @@
+package service;
+
+public class GameService {
+    public ListGamesResult listGames(ListGamesRequest listGamesRequest) {}
+    public JoinGameResult joinGame(JoinGameRequest joinGameRequest) {}
+    public CreateGameResult createGame(CreateGameRequest createGameRequest) {}
+}
