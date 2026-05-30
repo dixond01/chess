5632dabe50df88e598b6d8189f2d3517af396f22
Darbi <darbiadixon@gmail.com>
2026-05-30 16:17:32 -0600

    Add logout, createGame, listGames

--- client/src/main/java/client/PostLoginClient.java
+++ client/src/main/java/client/PostLoginClient.java
@@ -1,12 +1,21 @@
 package client;
 
+import chess.ChessGame;
 import model.DataAccessException;
+import model.GameData;
 import server.ServerFacade;
+import service.request.CreateGameRequest;
+import service.request.ListGamesRequest;
 import service.request.LogoutRequest;
+import service.result.CreateGameResult;
+import service.result.ListGamesResult;
+
+import java.util.ArrayList;
 
 public class PostLoginClient implements Client{
 
     private final ServerFacade server;
+    private ArrayList<GameData> gamesList;
 
     public PostLoginClient(ServerFacade server) {
         this.server = server;
@@ -35,7 +44,7 @@ public class PostLoginClient implements Client{
         return switch (cmd) {
             case ("logout") -> logout();
             case ("create") -> createGame(params);
-            case ("list") -> listGames(params);
+            case ("list") -> listGames();
             case("play") -> playGame(params);
             case("observe") -> observeGame(params);
             case("help") -> help();
@@ -46,5 +55,30 @@ public class PostLoginClient implements Client{
     private String logout() throws DataAccessException {
         server.logout(new LogoutRequest(server.getAuthToken()));
         server.setAuthToken(null);
+        new PreLoginClient(server).run();
+        return null;
     }
+
+    private String createGame(String[] params) throws DataAccessException {
+        if (params.length < 1) {
+            return "Please include a game name.";
+        }
+        CreateGameResult createGameResult = server.createGame(new CreateGameRequest(server.getAuthToken(), params[0]));
+        gamesList.add(new GameData(createGameResult.gameID(), null, null, params[0], new ChessGame()));
+        return String.format("Your gameID is: %d", gamesList.size());
+    }
+
+    private String listGames() throws DataAccessException {
+        ListGamesResult listGamesResult = server.listGames(new ListGamesRequest(server.getAuthToken()));
+        gamesList = (ArrayList<GameData>) listGamesResult.games();
+        StringBuilder gamesString = new StringBuilder(100);
+        if (gamesList.isEmpty()) {
+            return "No games to display.";
+        }
+        for (int i = 0; i < gamesList.size(); i++) {
+            gamesString.append(String.format("[%d]: %s ", i, gamesList.get(i).toString()));
+        }
+        return gamesString.toString();
+    }
+
 }
--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -3,4 +3,8 @@ package model;
 import chess.ChessGame;
 
 public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
+    @Override
+    public String toString() {
+        return String.format("Name: %s, White Player: %s, Black Player: %s", gameName, whiteUsername, blackUsername);
+    }
 }
