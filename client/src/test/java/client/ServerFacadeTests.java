b1fd3aa39f9a73cc9a35df18da970148fc954663
Darbi <darbiadixon@gmail.com>
2026-06-02 17:27:36 -0600

    Pass first test

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,26 @@
 package client;
 
+import model.DataAccessException;
 import org.junit.jupiter.api.*;
 import server.Server;
+import server.ServerFacade;
+import service.request.RegisterRequest;
+import service.result.RegisterResult;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade serverFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        serverFacade = new ServerFacade(String.valueOf(port));
     }
 
     @AfterAll
@@ -20,10 +28,16 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+    @BeforeEach
+    void clear() throws DataAccessException {
+        serverFacade.clear();
+    }
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    public void registerSuccess() throws DataAccessException {
+        RegisterResult actual = serverFacade.register(new RegisterRequest("username", "password", "email"));
+        RegisterResult expected = new RegisterResult("username", "token");
+        assertEquals(expected.username(), actual.username());
     }
 
 }
